bb99c941cf1427cb4a40ab9323dc6bb27d4eee4d
yang <1762086020@qq.com>
2019-10-29 18:53:10 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            你好 世界
+        </div>
+    );
+}
+
+export default App;
---
+++ src/setupProxy.js
@@ -0,0 +1,7 @@
+// 这里属于配置代理  建议在这里进行配置， 而不是在json 文件
+
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(proxy('/api', { target: 'http://localhost:5000/' }));
+};
